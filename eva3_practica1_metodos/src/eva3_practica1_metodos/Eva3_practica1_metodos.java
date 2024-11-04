1650c9ed457868a2682fb3ef66f2a2ccd5ee600a
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-11-04 17:40:33 -0600

    Add files via upload

---
+++ eva3_practica1_metodos/src/eva3_practica1_metodos/Eva3_practica1_metodos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_practica1_metodos;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva3_practica1_metodos {
+//uso de metodos:
+    //invocacion o llamda
+   
+    public static void main(String[] args) {
+        imprimirMensaje("HOLA MUNDO");
+    }
+    //DECLARACION
+    //header{encabezado}
+    //imprima un mensaje
+    //java: iniciar en minusculas,verbo,siguiente palabras inician enmayusculas
+    public static void imprimirMensaje(String mensaje)
+    {
+        
+        System.out.println("tu mensaje"+mensaje);
+    }
+}
