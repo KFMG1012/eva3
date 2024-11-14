e3e6210bdaf29b2c496d51b69b56511d02f0b6f8
KFMG1012 <L24550343@chihuahua2.tecnm.mx>
2024-11-14 16:43:34 -0600

    Add files via upload

---
+++ eva3_4metodos/src/eva3_4metodos/Eva3_4metodos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_4metodos;
+
+/**
+ *
+ * @author 52452
+ */
+public class Eva3_4metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        System.out.println(EncontrarMayor(10,15));
+        
+       //Valor grende enteros 
+    }
+    public static int EncontrarMayor (int num1,int num2)
+    {
+        int resu;
+        if (num1>num2){
+            resu=num1;
+        }
+        else{
+            resu=num2;
+        }
+            return resu;
+        
+    }
+    
+    
+    
+    //METODO QUE REGISTRE EL DIA DE LA SEMANA
+    //(1,7)lunes,martes,miercoles,jueves,viernes,sabado,miercoles
+    
+    
+    
+}
